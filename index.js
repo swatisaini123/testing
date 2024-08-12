b55afe7e0830149a1d576861c0ea92c25d838d34
swatisaini123 <113428077+swatisaini123@users.noreply.github.com>
2024-08-12 12:29:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.write('A Monk in Cloud'); //write a response to the client
   res.end(); //end the response
-}).listen(80); //the server object listens on port 80
-console.log("Port: 81");
+}).listen(400); //the server object listens on port 80
+console.log("Port: 83");
